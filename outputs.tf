a1719b903a40ed972158d1a2ab6931ade456ddb7
Alastair Munro <alastair@alastair-munro.com>
2020-05-22 13:13:19 +0100

    Added some more outputs.

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,32 @@ output "masters_public_ips" {
   value = [aws_instance.master.*.public_ip]
 }
 
+output "masters_private_ips" {
+  value = [aws_instance.master.*.private_ip]
+}
+
 output "agents_public_ips" {
   value = [aws_instance.agent.*.public_ip]
 }
+
+output "agents_private_ips" {
+  value = [aws_instance.agent.*.private_ip]
+}
+
+output "bastion_public_ip" {
+  value = [aws_instance.bastion.*.public_ip]
+}
+
+output "bastion_private_ip" {
+  value = [aws_instance.bastion.*.private_ip]
+}
+
 output "server_ssh_key" {
   value = var.k3s_key_pair
+}
+
+output "bastion_ssh_key" {
+  value = var.b_key_pair
 }
 
 output "lb_dns_name" {
