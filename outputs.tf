ff82b80c6e84b811e7e5849e24f0058324b7315d
Alastair Munro <alastair@alastair-munro.com>
2020-05-26 23:42:52 +0100

    Added instance ids to outputs so we can the id of master0.

--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,10 @@ output "masters_private_ips" {
   value = [aws_instance.master.*.private_ip]
 }
 
+output "masters_instance_ids" {
+  value = [aws_instance.master.*.id]
+}
+
 output "agents_public_ips" {
   value = [aws_instance.agent.*.public_ip]
 }
@@ -15,12 +19,20 @@ output "agents_private_ips" {
   value = [aws_instance.agent.*.private_ip]
 }
 
+output "agents_instance_ids" {
+  value = [aws_instance.agent.*.id]
+}
+
 output "bastion_public_ip" {
   value = [aws_instance.bastion.*.public_ip]
 }
 
 output "bastion_private_ip" {
   value = [aws_instance.bastion.*.private_ip]
+}
+
+output "bastion_instance_id" {
+  value = [aws_instance.bastion.*.id]
 }
 
 output "server_ssh_key" {
